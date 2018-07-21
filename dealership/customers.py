c97f98db9328262222b314d5388e4cb0e6c2919a
leon8941 <leon8941@gmail.com>
2018-07-21 01:29:49 -0400

    Created is_employee method.

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,11 +1,20 @@
 class Person(object):
     def __init__(self, first_name, last_name, email):
-        pass
-
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+      
+    def is_employee(object):
+      if type(object) is Customer:
+        return False
+      else:
+        return True
 
 class Customer(Person):
     pass
 
-
 class Employee(Person):
     pass
+
+John = Customer('Leon', 'Miew', 'leon@gmail.com')
+
