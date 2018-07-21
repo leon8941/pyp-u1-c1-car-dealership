56b5adfe37a67c24913ff488af6c89dfee0cc9ce
leon8941 <leon8941@gmail.com>
2018-07-21 02:35:00 -0400

    Create vehicles sales price and purchase price methods.

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,7 +1,34 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
-
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+          
+    def sale_price(object):
+        multiplier = 1.0
+        
+        if type(object) is Car:
+          multiplier = 1.2
+        elif type(object) is Truck:
+          multiplier = 1.6
+        else:
+          multiplier = 1.1
+        
+        return object.base_price * multiplier
+      
+    def purchase_price(object):
+        multiplier = 1.0
+        
+        if type(object) is Car:
+          multiplier = 0.004
+        elif type(object) is Truck:
+          multiplier = 0.02
+        else:
+          multiplier = 0.009
+        
+        return object.sale_price() - (multiplier * object.miles)
 
 class Car(Vehicle):
     pass
